88671668976fa49c4724402927c0384a466bf5eb
Hassan Lakis <lakis004@umn.edu>
2018-11-11 10:48:16 -0600

    claim home modified with plan status, still need to fix sidnav2 on locationInfo page

--- Assets/uploadPaperwork.js
+++ Assets/uploadPaperwork.js
@@ -1,5 +1,5 @@
 function uploadNewPaperwork() {
-    var x = document.createElement("INPUT");
+    var x = document.createElement("INPUT") && document.getElementById("attachPw").appendChild("x");
     x.setAttribute("type", "file");
     document.body.appendChild(x);
 }
