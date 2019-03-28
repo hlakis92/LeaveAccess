b2b7fd62fe9f1c46b1b5832f05f9dc683556b020
Hassan Lakis <lakis004@umn.edu>
2019-03-27 19:37:14 -0500

    new cont page

--- Assets/uploadPaperwork.js
+++ Assets/uploadPaperwork.js
@@ -1,5 +1,6 @@
 function uploadNewPaperwork() {
-    var x = document.createElement("INPUT") && document.getElementById("attachPw").appendChild("x");
-    x.setAttribute("type", "file");
-    document.body.appendChild(x);
+    var newDoc = document.createElement("INPUT") && document.getElementById("attachPw").appendChild("newDoc");
+    newDoc.setAttribute("type", "file");
+    document.body.appendChild(newDoc);
 }
+
