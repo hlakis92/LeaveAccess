4480b85e399bfc3a114152b94060a2da2520a77d
Manish Bhuva <manish.bhuvait@gmail.com>
2020-02-02 19:57:13 +0530

    AV: tasks and notes changes
    AV: tasks and notes changes

--- server/api/task/index.js
+++ server/api/task/index.js
@@ -8,11 +8,11 @@ module.exports = router;
 
 
 router.get('/tasks', controller.tasks);
-//router.post('/deletetask', controller.deletetask);
+// router.post('/deletetask', controller.deletetask);
 router.post('/addtask', controller.addtask);
-//router.post('/updatetask', controller.updatetask);
+// router.post('/updatetask', controller.updatetask);
 
-// router.get('/notes', controller.notes);
-//router.post('/deletetask', controller.deletetask);
-// router.post('/addnotes', controller.addnotes);
-//router.post('/updatetask', controller.updatetask);
+router.get('/notes', controller.notes);
+// router.post('/deletetask', controller.deletetask);
+router.post('/addnotes', controller.addnotes);
+// router.post('/updatetask', controller.updatetask);
